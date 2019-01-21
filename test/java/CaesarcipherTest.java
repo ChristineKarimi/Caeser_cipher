4c3b1b8e0c2f201d73cbbbceed0aff2b4857695a
christinekarimi <karimikim3@gmail.com>
2019-01-21 11:30:58 +0300

    Made some changes on the tests

--- test/java/CaesarcipherTest.java
+++ test/java/CaesarcipherTest.java
@@ -7,13 +7,13 @@ public class CipherTest {
     @Test
     public void Cipher_testingEncryptingmethod() {
         Cipher cipher =new Cipher();
-        assertEquals("nbe",cipher.encrypt("mad",1));
+        assertEquals("fb",cipher.encrypt("fbi",1));
     }
 
     @Test
     public void Cipher_testingDecryptingMethod() {
         Cipher cipher =new Cipher();
-        assertEquals("mad",cipher.decrypt("nbe",1));
+        assertEquals("fbi",cipher.decrypt("fb",1));
     }
 
 
